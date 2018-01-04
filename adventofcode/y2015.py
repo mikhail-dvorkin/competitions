c0e028fa846c8f86f4ec9d9c398e64e620e1910c
Mikhail Dvorkin <mikhail.dvorkin@gmail.com>
2018-01-05 02:39:50 +0300

    solution for day5

--- adventofcode/y2015.py
+++ adventofcode/y2015.py
@@ -2,6 +2,7 @@
 import data
 import hashlib
 import itertools
+import re
 
 def day1(s):
 	yield s.count('(') - s.count(')')
@@ -26,6 +27,16 @@ def day3(s):
 
 def day4(s):
 	yield next(i for i in itertools.count() if hashlib.md5(bytes(s + str(i), 'utf8')).hexdigest().startswith('00000'))
+
+def day5(s):
+	res = '[aeiou]', '(.)\\1', 'ab|cd|pq|xy'
+	def good(line):
+		a, b, c = [len(re.findall(r, line)) for r in res]
+		return a >= 3 and b >= 1 and c == 0
+	yield len(list(filter(good, s.split())))
+
+def day6(s):
+	yield 0
 
 d = data.y2015
 for i in range(len(d) - 1, -1, -1):
