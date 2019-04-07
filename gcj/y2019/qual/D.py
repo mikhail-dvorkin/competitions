201ef2922407f50549cd1a40ef682524e2511e04
Mikhail Dvorkin <mikhail.dvorkin@gmail.com>
2019-04-08 01:28:33 +0300

    Better solution for Google Code Jam 2019 Qual Round problem D

--- gcj/y2019/qual/D.py
+++ gcj/y2019/qual/D.py
@@ -3,30 +3,17 @@ import sys
 
 def solve():
 	n, b, f = map(int, input().split())
+	output, data = [], []
 	for k in range(f):
-		group = 1 << (f - 1 - k)
-		output = ''.join([str(i // group % 2) for i in range(n)])
-		print(output)
+		output.append(''.join([str((i >> (f - 1 - k)) & 1) for i in range(n)]))
+		print(output[k])
 		sys.stdout.flush()
-		data = input()
-		if k == 0:
-			i_prev, broken = -1, []
-			for i in range(len(data)):
-				if i == len(data) - 1 or data[i] != data[i + 1]:
-					broken.append(group - (i - i_prev))
-					i_prev = i
-			groups = (n - 1) // group + 1
-			if len(broken) < groups:
-				broken.append(n - group * (groups - 1))
-			continue
-		cum, broken_new = 0, []
-		for i in range(len(broken)):
-			segment = data[cum:cum + 2 * group - broken[i]]
-			for c in '01':
-				broken_new.append(group - segment.count(c))
-			cum += len(segment)
-		broken = broken_new
-	print(*[i for i in range(n) if broken[i]])
+		data.append(input() + '$')
+	broken = []
+	for i in range(n):
+		if any([data[k][i - len(broken)] != output[k][i] for k in range(f)]):
+			broken.append(i)
+	print(*broken)
 	input()
 
 tests = int(input())
