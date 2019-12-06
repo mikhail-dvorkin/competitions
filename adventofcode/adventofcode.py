f27529d125ec96e9726de831eb752ca48ff4e465
Mikhail Dvorkin <mikhail.dvorkin@gmail.com>
2019-12-06 18:14:50 +0300

    Refactoring

--- adventofcode/adventofcode.py
+++ adventofcode/adventofcode.py
@@ -6,20 +6,25 @@ import sys
 
 sys.setrecursionlimit(max(10 ** 6, sys.getrecursionlimit()))
 
-def run(year=None, filename = 'data~.json', url='https://pastebin.com/raw/xGvU9SZY'):
+def run(year=None):
 	if not year:
 		import __main__
 		year = os.path.basename(__main__.__file__)[1:5]
 	year = str(year)
 	year_module = __import__('y' + year)
-	if not os.path.isfile(filename):
-		with open(filename, 'w') as f:
-			f.write(requests.get(url).text)
-	data = json.load(open(filename, 'r'))[year]
+	data = load_data()[year]
 	for i in range(len(data) - 1, -1, -1):
 		day = 'day' + str(i + 1)
 		f = getattr(year_module, day)
 		print(day, *f(data[i]))
 
+def load_data(filename='data~.json', url='https://pastebin.com/raw/xGvU9SZY'):
+	if not os.path.isfile(filename):
+		with open(filename, 'w') as f:
+			f.write(requests.get(url).text)
+	return json.load(open(filename, 'r'))
+
 if __name__ == '__main__':
-	run(2019)
+	for year in load_data():
+		print(year)
+		run(year)
