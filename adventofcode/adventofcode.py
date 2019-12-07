c1edd3664382e21b011600c16ceb2a075cb98c51
Mikhail Dvorkin <mikhail.dvorkin@gmail.com>
2019-12-08 02:01:33 +0300

    Output partial answers immediately

--- adventofcode/adventofcode.py
+++ adventofcode/adventofcode.py
@@ -16,7 +16,10 @@ def run(year=None):
 	for i in range(len(data) - 1, -1, -1):
 		day = 'day' + str(i + 1)
 		f = getattr(year_module, day)
-		print(day, *f(data[i]))
+		print(day, end=': ')
+		for ans in f(data[i]):
+			print(ans, end=' ', flush=True)
+		print()
 
 def load_data(filename='data~.json', url='https://pastebin.com/raw/xGvU9SZY'):
 	if not os.path.isfile(filename):
