ba8ba8aaa66ab63014af8e5264bc256e7d0c7626
Mikhail Dvorkin <mikhail.dvorkin@gmail.com>
2020-03-05 00:17:59 +0300

    Refactor 2020 Humblefool Cup Prelims solutions

--- topcoder/humblefool2020/NonSimilarTriangles.java
+++ topcoder/humblefool2020/NonSimilarTriangles.java
@@ -4,18 +4,16 @@ import java.util.*;
 
 public class NonSimilarTriangles {
 	public int count(int[] a) {
+		Arrays.sort(a);
 		int n = a.length;
 		HashSet<String> ans = new HashSet<>();
 		for (int i = 0; i < n; i++) {
-			for (int j = i + 1; j < n; j++) {
-				for (int k = j + 1; k < n; k++) {
+			for (int j = 0; j < i; j++) {
+				for (int k = 0; k < j; k++) {
 					int[] b = new int[] {a[i], a[j], a[k]};
-					Arrays.sort(b);
-					if (b[2] >= b[0] + b[1]) continue;
+					if (b[0] >= b[1] + b[2]) continue;
 					int g = gcd(b[0], gcd(b[1], b[2]));
-					b[0] /= g;
-					b[1] /= g;
-					b[2] /= g;
+					b[0] /= g; b[1] /= g; b[2] /= g;
 					ans.add(Arrays.toString(b));
 				}
 			}
