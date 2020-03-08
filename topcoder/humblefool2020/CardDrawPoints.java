459420c76090a710171f083e9122599e3f16607e
Mikhail Dvorkin <mikhail.dvorkin@gmail.com>
2020-03-08 12:07:42 +0300

    Refactor 2020 Humblefool Cup Prelims solutions

--- topcoder/humblefool2020/CardDrawPoints.java
+++ topcoder/humblefool2020/CardDrawPoints.java
@@ -4,24 +4,19 @@ public class CardDrawPoints {
 	public double expectedScore(int[] count) {
 		int n = count.length;
 		double[] a = new double[1 << n];
-		main:
 		for (int mask = a.length - 1; mask >= 0; mask--) {
 			int options = 0;
 			int have = 0;
-			double canHave = 0;
 			for (int i = 0; i < n; i++) {
-				int c = count[i];
-				if (((mask >> i) & 1) == 1) {
-					c--;
-					have += i;
-				} else {
-					canHave += a[mask | (1 << i)] * c;
+				options += count[i];
+				if (((mask >> i) & 1) == 0) {
+					a[mask] += a[mask | (1 << i)] * count[i];
+					continue;
 				}
-				if (c < 0) continue main;
-				options += c;
+				have += i;
+				options--;
 			}
-			if (options == 0) canHave = 0; else canHave /= options;
-			a[mask] = Math.max(have, canHave);
+			a[mask] = options == 0 ? have : Math.max(have, a[mask] / options);
 		}
 		return a[0];
 	}
