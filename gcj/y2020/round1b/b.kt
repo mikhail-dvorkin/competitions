8bc59da819c018d1523328547a0d9d4612f98bfe
Mikhail Dvorkin <mikhail.dvorkin@gmail.com>
2020-04-20 16:03:22 +0300

    Refactor Google Code Jam 2020 Round 1B solutions

--- gcj/y2020/round1b/b.kt
+++ gcj/y2020/round1b/b.kt
@@ -5,9 +5,7 @@ import kotlin.math.abs
 const val SIZE = 1_000_000_000
 
 private fun solve() {
-	val gridSize = 9
-	val xs = (1 until gridSize).map { -SIZE + 2 * SIZE / gridSize * it }
-	val grid = xs.flatMap { x -> xs.map { y -> x to y } }
+	val grid = listOf(-SIZE / 3, SIZE / 3).cartesianSquare()
 	val (xInit, yInit) = grid.first { (x, y) -> hit(x, y) }
 	val (xLeft, _) = furthestInside(xInit, yInit, -1, 0)
 	val (xRight, _) = furthestInside(xInit, yInit, 1, 0)
@@ -18,9 +16,9 @@ private fun solve() {
 }
 
 private fun furthestInside(x: Int, y: Int, dx: Int, dy: Int): Pair<Int, Int> {
-	var (low, high) = 0 to 2 * SIZE
+	var (low, high) = 0 to 2 * SIZE + 1
 	while (low + 1 < high) {
-		val mid = ((low + high.toLong()) / 2).toInt()
+		val mid = low + (high - low) / 2
 		if (hit(x + dx * mid, y + dy * mid)) low = mid else high = mid
 	}
 	return x + dx * low to y + dy * low
@@ -34,8 +32,7 @@ private fun hit(x: Int, y: Int): Boolean {
 
 private class Found : Exception()
 
-fun main() = repeat(readInts()[0]) { try { solve() } catch (_: Found) {} }
+fun main() = repeat(readLn().split(" ")[0].toInt()) { try { solve() } catch (_: Found) {} }
 
 private fun readLn() = readLine()!!
-private fun readStrings() = readLn().split(" ")
-private fun readInts() = readStrings().map { it.toInt() }
+private fun <T> Iterable<T>.cartesianSquare() = flatMap { x -> map { y -> x to y } }
