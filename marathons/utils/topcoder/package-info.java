2af5cc8af1d6d5245fc384f30ba1d8d4a4bd20b4
Mikhail Dvorkin <mikhail.dvorkin@gmail.com>
2020-08-02 02:09:47 +0300

    (not my code) Add TopCoder's common code for marathon visualizers

---
+++ marathons/utils/topcoder/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Code published by TopCoder.
+ *
+ * Current version taken from Marathon Match 119 in Aug 2020.
+ *
+ * https://www.topcoder.com/challenges/30134026
+ */
+package marathons.utils.topcoder;
