9742d60ba054e6cb9e805e96d632ee7fe94fed45
Mikhail Dvorkin <mikhail.dvorkin@gmail.com>
2021-10-18 01:13:11 +0300

    Store only one copy of TopCoder's common code plus one patched copy

--- marathons/utils/topcoder2021_09_15/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Code published by TopCoder.
- *
- * Version taken from Marathon Match 129 in Sep 2021.
- *
- * https://www.topcoder.com/challenges/c522c9e3-29d3-4788-98db-05748f0842dc
- */
-package marathons.utils.topcoder2021_09_15;
