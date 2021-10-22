9d6e8997892d9a7f17638e10d225e760f5de170c
Mikhail Dvorkin <mikhail.dvorkin@gmail.com>
2021-10-22 10:11:24 +0300

    Add myGen parameter for non-standard test generation

--- marathons/utils/topcoderMy/Parameters.java
+++ marathons/utils/topcoderMy/Parameters.java
@@ -18,6 +18,7 @@ public class Parameters {
 	public static final String loadSolOutput = "loadSolOutput";
 	public static final String myExec = "myExec";
 	public static final String myVis = "myVis";
+	public static final String myGen = "myGen";
 	public static final String noAntialiasing = "noAntialiasing";
 	public static final String noOutput = "noOutput";
 	public static final String noSummary = "noSummary";
