269f78db8b478536240555fdf602e922a079a4ad
Mikhail Dvorkin <mikhail.dvorkin@gmail.com>
2021-11-23 14:01:30 +0300

    Add CodeChef SnackDown 2021 Online Pre-Elimination Round upsolving

--- codechef/snackdown2021/preelim/slippers.kt
+++ codechef/snackdown2021/preelim/slippers.kt
@@ -1,36 +1,23 @@
 package codechef.snackdown2021.preelim
 
-import java.util.*
+private const val MAX_S = 200_000
 
 @Suppress("UNUSED_PARAMETER") // Kotlin 1.2
 fun main(args: Array<String>) {
 	readLn()
 	val a = readInts()
-	val prev = IntArray(a.size)
-	val treeMap = TreeMap<Int, Int>()
-	for (i in a.indices) {
-		val entry = treeMap.ceilingEntry(a[i])
-		if (entry == null) {
-			prev[i] = -1
-		} else {
-			prev[i] = entry.value
-		}
-		treeMap[a[i]] = i
-	}
-	val dp = MutableList(a.size) { 1.toModular() }
+	val fenwickTree = FenwickTree(MAX_S + 1)
 	var ans = 1.toModular()
-	for (i in a.indices.reversed()) {
-		dp[i] = 1 + dp[i]
-		if (prev[i] == -1) {
-			ans *= dp[i]
-			continue
-		}
-		dp[prev[i]] *= dp[i]
+	for (x in a) {
+		val added = 1 + fenwickTree.sum(x).toModular()
+		fenwickTree[x] = (fenwickTree[x].toInt() + added).x.toLong()
+		ans += added
 	}
 	println(ans)
 }
 
 private fun Int.toModular() = Modular(this)//toDouble()
+private fun Long.toModular() = Modular(this)//toDouble()
 private class Modular {
 	companion object {
 		const val M = 998244353
@@ -38,6 +25,7 @@ private class Modular {
 	val x: Int
 	@Suppress("ConvertSecondaryConstructorToPrimary")
 	constructor(value: Int) { x = (value % M).let { if (it < 0) it + M else it } }
+	constructor(value: Long) { x = (value % M).toInt().let { if (it < 0) it + M else it } }
 	operator fun plus(that: Modular) = Modular((x + that.x) % M)
 	operator fun minus(that: Modular) = Modular((x + M - that.x) % M)
 	operator fun times(that: Modular) = (x.toLong() * that.x % M).toInt().toModular()
@@ -49,6 +37,44 @@ private operator fun Int.plus(that: Modular) = Modular(this) + that
 private operator fun Int.minus(that: Modular) = Modular(this) - that
 private operator fun Int.times(that: Modular) = Modular(this) * that
 private operator fun Int.div(that: Modular) = Modular(this) / that
+
+class FenwickTree(n: Int) {
+	var t: LongArray
+	fun add(i: Int, value: Long) {
+		var j = i
+		while (j < t.size) {
+			t[j] += value
+			j += j + 1 and -(j + 1)
+		}
+	}
+
+	fun sum(i: Int): Long {
+		var j = i
+		var res: Long = 0
+		j--
+		while (j >= 0) {
+			res += t[j]
+			j -= j + 1 and -(j + 1)
+		}
+		return res
+	}
+
+	fun sum(start: Int, end: Int): Long {
+		return sum(end) - sum(start)
+	}
+
+	operator fun get(i: Int): Long {
+		return sum(i, i + 1)
+	}
+
+	operator fun set(i: Int, value: Long) {
+		add(i, value - get(i))
+	}
+
+	init {
+		t = LongArray(n)
+	}
+}
 
 private fun readLn() = readLine()!!.trim()
 private fun readStrings() = readLn().split(" ")
