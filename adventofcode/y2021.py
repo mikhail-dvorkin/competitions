7b6ce9a65855022be63e698ff88d069e3ba4275f
Mikhail Dvorkin <mikhail.dvorkin@gmail.com>
2021-12-01 11:37:03 +0300

    Add Advent of Code 2021 solution for day1.2

--- adventofcode/y2021.py
+++ adventofcode/y2021.py
@@ -2,9 +2,12 @@
 import adventofcode
 
 
-def day1(s):
+def day1(s, windows=[1, 3]):
 	s = list(map(int, s.split('\n')))
-	yield len([(a, b) for (a, b) in zip(s, s[1:]) if a < b])
+	def increases(array):
+		return sum([a < b for (a, b) in zip(array, array[1:])])
+	for window in windows:
+		yield increases([sum(s[i:i + window]) for i in range(len(s) - window + 1)])
 
 
 if __name__ == '__main__':
