80c78e474444d20d76abf7c51cd98889f427cc12
Mikhail Dvorkin <mikhail.dvorkin@gmail.com>
2021-12-02 12:38:33 +0300

    Add Advent of Code 2021 solution for day2.1

--- adventofcode/y2021.py
+++ adventofcode/y2021.py
@@ -9,6 +9,17 @@ def day1(s, windows=[1, 3]):
 	for window in windows:
 		yield increases([sum(s[i:i + window]) for i in range(len(s) - window + 1)])
 
+def day2(s):
+	DIR = {'forward': (1, 0), 'up': (0, 1), 'down': (0, -1)}
+	s = s.split('\n')
+	x, y = 0, 0
+	for line in s:
+		d, length = line.split()
+		length = int(length)
+		dx, dy = [length * z for z in DIR[d]]
+		x += dx; y += dy
+	yield x * y
+
 
 if __name__ == '__main__':
 	adventofcode.run()
