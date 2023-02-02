c9a4e025987f524cb17c79c510af3d7ce47ec148
Mikhail Dvorkin <mikhail.dvorkin@gmail.com>
2023-02-02 21:04:20 +0300

    Add package info for AtCoder Heuristic Contest 17, Third 2022: Road Repair

---
+++ marathons/atcoder/heuristic17_third2022_roadRepair/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * https://atcoder.jp/contests/ahc017/tasks/ahc017_a
+ *
+ * THIRD PROGRAMMING CONTEST 2022 (AtCoder Heuristic Contest 017)
+ *
+ * Road Repair
+ */
+package marathons.atcoder.heuristic17_third2022_roadRepair;
