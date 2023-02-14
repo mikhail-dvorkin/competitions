5cbc34b4d0e4096d9d5f56a72a097ce2cfbcd576
Mikhail Dvorkin <mikhail.dvorkin@gmail.com>
2023-02-14 23:24:30 +0300

    Add assertion check

---
+++ marathons/utils/JavaUtils.java
@@ -0,0 +1,13 @@
+package marathons.utils;
+
+public class JavaUtils {
+	public static void requireEnablesAssertions() {
+		int assertOn = 0;
+		//noinspection AssertWithSideEffects,ConstantConditions
+		assert (assertOn = 1) > 0;
+		//noinspection ConstantConditions
+		if (assertOn == 0) {
+			throw new AssertionError("Asserts must be on.");
+		}
+	}
+}
