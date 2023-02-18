e98450fa1584d58f54c935e1d2827c003bff42f7
Mikhail Dvorkin <mikhail.dvorkin@gmail.com>
2023-02-18 20:00:14 +0300

    Add package info for AtCoder Heuristic Contest 18, Recruit Nihonbashi Half Marathon 2023 Winter: Excavation

---
+++ marathons/atcoder/ahc18_nihonbashi2023winter_excavation/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * https://atcoder.jp/contests/ahc018/tasks/ahc018_a
+ *
+ * RECRUIT Nihonbashi Half Marathon 2023 Winter (AtCoder Heuristic Contest 018)
+ *
+ * Excavation
+ */
+package marathons.atcoder.ahc18_nihonbashi2023winter_excavation;
