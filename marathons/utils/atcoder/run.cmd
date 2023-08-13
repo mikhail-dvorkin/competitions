703c73789645a9a9e1a70e816e2a7d1e660365dc
Mikhail Dvorkin <mikhail.dvorkin@gmail.com>
2023-08-13 16:57:31 +0300

    Refactor Windows runner

--- marathons/utils/atcoder/run.cmd
+++ marathons/utils/atcoder/run.cmd
@@ -1,6 +1,11 @@
+setlocal enabledelayedexpansion
 cd tools~/windows
+set IMG_DIR=../img
 FOR /L %%i IN (0,1,9) DO (
-	tester.exe java -jar ../../solution~.jar < ../in/000%%i.txt > ../out/000%%i.txt
-	vis.exe ../in/000%%i.txt ../out/000%%i.txt
-	move vis.png ../img/000%%i.png
+	set test=0000%%i
+	set test=!test:~-4!
+	tester.exe java -jar ../../solution~.jar < ../in/!test!.txt > ../out/!test!.txt
+	vis.exe ../in/!test!.txt ../out/!test!.txt
+	move vis.png %IMG_DIR%/!test!.png
+	move vis.html %IMG_DIR%/!test!.html
 )
