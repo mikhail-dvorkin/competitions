2772b8f7905551db35e837441ddce975eeee17db
Mikhail Dvorkin <mikhail.dvorkin@gmail.com>
2023-08-17 15:40:22 +0300

    Use .kt.txt extension for submitted solution

--- marathons/utils/atcoder/compile.sh
+++ marathons/utils/atcoder/compile.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-rm solution~.jar solution_submit~.kt
+rm solution~.jar solution_submit~.kt solution_submit~.kt.txt
 cat $1 | sed -E 's/^(.*\/\/TESTING\s*)$/\/\/\1/' | sed -E 's/^\/\/(.*\/\/SUBMISSION\s*)$/\1/' > solution_submit~.kt
 kotlinc -include-runtime -d solution~.jar solution_submit~.kt
+mv solution_submit~.kt solution_submit~.kt.txt
