36cdcd0aec7f06c2f27b23a1b8bf11f854dd7a4f
Mikhail Dvorkin <mikhail.dvorkin@gmail.com>
2023-08-17 15:40:47 +0300

    Use .out extension for output files

--- marathons/utils/atcoder/run.cmd
+++ marathons/utils/atcoder/run.cmd
@@ -5,8 +5,8 @@ set IMG_DIR=../img
 FOR /L %%i IN (0,1,9) DO (
 	set test=0000%%i
 	set test=!test:~-4!
-	tester.exe java -jar ../../solution~.jar < ../in/!test!.txt > ../out/!test!.txt
-	vis.exe ../in/!test!.txt ../out/!test!.txt
+	tester.exe java -jar ../../solution~.jar < ../in/!test!.txt > ../out/!test!.out
+	vis.exe ../in/!test!.txt ../out/!test!.out
 	move vis.png %IMG_DIR%/!test!.png > nul 2>nul
 	move vis.html %IMG_DIR%/!test!.html > nul 2>nul
 	echo.
