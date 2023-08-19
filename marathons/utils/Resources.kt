bc3cb0f14b7ec567cb135ab232af7db518ff09d7
Mikhail Dvorkin <mikhail.dvorkin@gmail.com>
2023-08-19 12:19:17 +0300

    Refactor non-atcoder-specific code out

--- marathons/utils/Resources.kt
+++ marathons/utils/Resources.kt
@@ -5,3 +5,13 @@ import java.io.File
 fun resourcePrefix() = "res/" + Evaluator._project + "/"
 
 fun extensionOfFile(f: File) = f.extension
+
+val isWindows = System.getProperty("os.name").lowercase().startsWith("windows")
+
+fun exec(command: String, dir: String): Pair<String, String> {
+	val processBuilder = ProcessBuilder(*command.split(" ").toTypedArray()).directory(File(dir))
+	val process = processBuilder.start()
+	val output = process.inputStream.reader().readText()
+	val error = process.errorStream.reader().readText()
+	return Pair(output, error)
+}
--- marathons/utils/atcoder/Visualizer.kt
+++ marathons/utils/atcoder/Visualizer.kt
@@ -94,17 +94,7 @@ fun atcoderVisualizer(
 	return Callable<Void?> { runAndVisualizeTheir(solution, isInteractive); null }
 }
 
-val isWindows = System.getProperty("os.name").lowercase().startsWith("windows")
-
 fun rustExe(programName: String) = if (isWindows)
 	"cmd /c windows\\$programName.exe"
 else
 	"cargo run --release --bin $programName"
-
-fun exec(command: String, dir: String): Pair<String, String> {
-	val processBuilder = ProcessBuilder(*command.split(" ").toTypedArray()).directory(File(dir))
-	val process = processBuilder.start()
-	val output = process.inputStream.reader().readText()
-	val error = process.errorStream.reader().readText()
-	return Pair(output, error)
-}
