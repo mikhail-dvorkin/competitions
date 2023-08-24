4e781da37faf8c82183f42d2789a78963f9d79c3
Mikhail Dvorkin <mikhail.dvorkin@gmail.com>
2023-08-24 23:24:58 +0300

    Add package info and score logs for AtCoder Heuristic Contest 22: Exploring Another Space

---
+++ marathons/atcoder/ahc22_nihonbashi2023summer_exploringAnotherSpace/package-info.java
@@ -0,0 +1,43 @@
+/**
+ * https://atcoder.jp/contests/ahc022/tasks/ahc022_a
+ *
+ * RECRUIT Nihonbashi Half Marathon 2023 Summer (AtCoder Heuristic Contest 022)
+ *
+ * Exploring Another Space
+ */
+package marathons.atcoder.ahc22_nihonbashi2023summer_exploringAnotherSpace;
+/*
+Score logs:
+
+5864905 → 316th, 220933627
+6158940
+7209816
+angle:
+4780063
+3577812
+4167069
+4804833
+3526827
+4884849
+7898983
+7829998
+8170864 → 65th, 419432803
+8212763
+8267918
+9688684 level1=0.7
+17855781 2N, (1+2N)/2
+28782368 2N, 2N
+17917116
+29800060 → 35th, 1238310579
+12024304 random, random
+30716999 smoothing
+33821444 paramOpt, 1 hour → 40th, 1352141461
+33326070 paramOpt, 5 hour → 41st, 1349021007
+33910680 k+1,k+1
+33965310 atDiagonal=0.7 → 41st, 1351931762
+34020315 gradient after stripe
+35365812 pConfidence=3
+36290279 pConfidence=2
+36932134 pConfidence=ln(W) - 2.5
+35460956 pConfidence=ln(W) - 1 → 34th, 1402731819
+ */
