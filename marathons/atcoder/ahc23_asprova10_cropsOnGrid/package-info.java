347bbd1a9a871b22680c730046b88eb29f9f575e
Mikhail Dvorkin <mikhail.dvorkin@gmail.com>
2023-09-10 22:38:56 +0300

    Add package info and score log for AtCoder Heuristic Contest 23, 10th Asprova Programming Contest: Crops on Grid

---
+++ marathons/atcoder/ahc23_asprova10_cropsOnGrid/package-info.java
@@ -0,0 +1,34 @@
+package marathons.atcoder.ahc23_asprova10_cropsOnGrid;
+/*
+775552  → 38897750 — 85th
+782692  mid-segment connection in Steiner problem
+784751  centerness
+785053  improveA
+791567  group give-take
+800244  pCoverDist=7
+805362  pCoverDist=8
+808071  pCoverDist=9
+808649  pCoverDist=11
+809452  pCoverDist=10   → 40468550 — 89→33rd
+815625  use corridor, strictly same interval
+815628  pCoverDist=9    → 40796350 — 35→30th
+816849  prefer later ending in group
+818608  improveC, s=0..1
+818899  s=0..∞  → 40980625 — 29th
+819864  fixed logic of blocked
+820155  move crop in corridor to son
+819978  …to furthest ancestor
+820140  optimize planGroups
+822010  pCoverDist=6…11
+822262  pCoverDist=3…14 → 41124175 — 32→28th            avg886ms→server1995ms
+822262  6…14
+822259  7…14
+822346  7…15
+822372  pSaveForCorridor=3  → 41132025 — 28th           avg491ms→server1756ms
+824187  adhoc striped area  → 41187875 — 37th           avg579ms→server1869ms
+824382  pAdhocMOdd=9,11
+824528  pAdhocAllowDifference=0..2
+824781  pCoverDist=7…13, 9 iters
+825320  toTL    → 41237600 — 38th
+825480  bfsAll ran only once    → 41260250
+ */
