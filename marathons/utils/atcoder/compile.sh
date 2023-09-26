2284e7f4e9d0c319b1d2dd3f1eadf206d70187f6
Mikhail Dvorkin <mikhail.dvorkin@gmail.com>
2023-09-26 17:34:12 +0300

    Improve cleaning Kotlin solution for submission

--- marathons/utils/atcoder/compile.sh
+++ marathons/utils/atcoder/compile.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 rm solution~.jar solution_submit~.kt solution_submit~.kt.txt
-cat $1 | sed -E 's/^(.*\/\/TESTING\s*)$/\/\/\1/' | sed -E 's/^\/\/(.*\/\/SUBMISSION\s*)$/\1/' > solution_submit~.kt
+cat $1 \
+| sed -E 's/^(.*\/\/TESTING\s*)$/\/\/\1/' \
+| sed -E 's/^\/\/(.*\/\/SUBMISSION\s*)$/\1/' \
+| sed -E 's/^(package\s.*)$/\/\/\1/' \
+| sed -E 's/^(\s*require\(.*)$/\/\/\1/' \
+| sed -E 's/^(\s*log\s*\{.*\}\s*)$/\/\/\1/' \
+| sed -E 's/^(\s*info\s*\{.*\}\s*)$/\/\/\1/' \
+> solution_submit~.kt
 kotlinc -include-runtime -d solution~.jar solution_submit~.kt
 mv solution_submit~.kt solution_submit~.kt.txt
