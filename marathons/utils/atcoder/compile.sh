ea5d95f65639a4fe8ad086f01979e66ff03ec4bf
Mikhail Dvorkin <mikhail.dvorkin@gmail.com>
2023-10-05 11:01:22 +0300

    Allow spaces in controlling comments

--- marathons/utils/atcoder/compile.sh
+++ marathons/utils/atcoder/compile.sh
@@ -2,8 +2,8 @@
 
 rm solution~.jar solution_submit~.kt solution_submit~.kt.txt
 cat $1 \
-| sed -E 's/^(.*\/\/TESTING\s*)$/\/\/\1/' \
-| sed -E 's/^\/\/(.*\/\/SUBMISSION\s*)$/\1/' \
+| sed -E 's/^(.*\/\/\s*TESTING\s*)$/\/\/\1/' \
+| sed -E 's/^\/\/(.*\/\/\s*SUBMISSION\s*)$/\1/' \
 | sed -E 's/^(package\s.*)$/\/\/\1/' \
 | sed -E 's/^(\s*require\(.*)$/\/\/\1/' \
 | sed -E 's/^(\s*log\s*\{.*\}\s*)$/\/\/\1/' \
