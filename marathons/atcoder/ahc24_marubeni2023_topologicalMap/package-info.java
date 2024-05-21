f3c82c928f48acfcf0575b4bcd52c2caa5bed8da
Mikhail Dvorkin <mikhail.dvorkin@gmail.com>
2024-05-21 23:16:43 +0300

    Add useful links for future research

--- marathons/atcoder/ahc24_marubeni2023_topologicalMap/package-info.java
+++ marathons/atcoder/ahc24_marubeni2023_topologicalMap/package-info.java
@@ -5,6 +5,11 @@
  */
 package marathons.atcoder.ahc24_marubeni2023_topologicalMap;
 /*
+Articles that can help:
+DOI:10.1007/BFb0049394
+https://cs.brown.edu/people/rtamassi/gdhandbook/chapters/orthogonal.pdf
+https://neerc.ifmo.ru/wiki/index.php?title=%D0%93%D0%B0%D0%BC%D0%BC%D0%B0-%D0%B0%D0%BB%D0%B3%D0%BE%D1%80%D0%B8%D1%82%D0%BC
+
 FAILED_2o FAILED_2g FAILED_2g
 FAILED_k  FAILED_18 FAILED_18 FAILED_1t
 FAILED_25 FAILED_25 FAILED_25
