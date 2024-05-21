9c5c32230ca91476bed9835ed8101e21fb1056bd
Mikhail Dvorkin <mikhail.dvorkin@gmail.com>
2024-05-21 22:12:11 +0100

    Comment out obsolete NashornScriptEngineFactory

--- facebook/y2019/qual/c.kt
+++ facebook/y2019/qual/c.kt
@@ -1,14 +1,14 @@
 @file:Suppress("DEPRECATION")
 package facebook.y2019.qual
 
-import jdk.nashorn.api.scripting.NashornScriptEngineFactory
+//import jdk.nashorn.api.scripting.NashornScriptEngineFactory
 
 private fun solve(s: String): String {
 	val formulas = List(2) { s.replace('x', '0' + it).replace('X', '0' + 1 - it) }
 	return eval(formulas.joinToString("^"))
 }
 
-private fun eval(code: String) = NashornScriptEngineFactory().scriptEngine.eval(code).toString()
+private fun eval(code: String): Nothing = error("deprecated")// NashornScriptEngineFactory().scriptEngine.eval(code).toString()
 
 fun main() = repeat(readInt()) { println("Case #${it + 1}: ${solve(readLn())}") }
 
