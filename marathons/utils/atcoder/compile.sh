d68cd897316c29aa0e734ac485a04470675f1813
Mikhail Dvorkin <mikhail.dvorkin@gmail.com>
2025-09-30 20:05:18 +0100

    Add date to submitted solution

--- marathons/utils/atcoder/compile.sh
+++ marathons/utils/atcoder/compile.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 rm solution~.jar solution_submit~.kt solution_submit~.kt.txt
+echo "//`date -u`" > solution_submit~.kt
 cat $1 \
 | sed -E 's/^(.*\/\/\s*TESTING\s*)$/\/\/\1/' \
 | sed -E 's/^\/\/(.*\/\/\s*SUBMISSION\s*)$/\1/' \
@@ -8,6 +9,6 @@ cat $1 \
 | sed -E 's/^(\s*require\(.*)$/\/\/\1/' \
 | sed -E 's/^(\s*log\s*\{.*\}\s*)$/\/\/\1/' \
 | sed -E 's/^(\s*info\s*\{.*\}\s*)$/\/\/\1/' \
-> solution_submit~.kt
+>> solution_submit~.kt
 kotlinc -include-runtime -d solution~.jar solution_submit~.kt
 mv solution_submit~.kt solution_submit~.kt.txt
